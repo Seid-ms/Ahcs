e83827d655785e54970997931e2448d08d026dd9
seid-ms <muhammedseid08@gmail.com>
2021-05-07 02:22:11 +0300

    update

--- ahcs_project/hospital_admin/urls.py
+++ ahcs_project/hospital_admin/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
      path('hospital_admin_dashboard/',views.hospital_admin_dashboard,name='hospital_admin_dashboard'),
+     path('staff_registration/',views.add_staff,name='register_staff'),
 ]
--- ahcs_project/hospital_admin/views.py
+++ ahcs_project/hospital_admin/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def hospital_admin_dashboard(request):
     context={ }
     return render(request,"hospital_admin/hospital_admin_dashboard.html",context)
+def add_staff(request):
+    context={ }
+    return render(request,"forms/registration_form.html",context)
